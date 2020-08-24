03e148e28f3b292a81f1305e065b6758ebbb36f8
Miha Drofenik <miha.drofenik92@gmail.com>
2020-08-24 22:29:45 +0200

    Updates

---
+++ precache-manifest.f51818a2519bc335368e1335143ba9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cb50684d7827b092a9435862c24f53",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "47185c519547defb5182",
+    "url": "/my-portfolio/static/css/main.d861fe02.chunk.css"
+  },
+  {
+    "revision": "6c8a82d82873a64c3db6",
+    "url": "/my-portfolio/static/js/2.526aafd1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-portfolio/static/js/2.526aafd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "47185c519547defb5182",
+    "url": "/my-portfolio/static/js/main.029a25f0.chunk.js"
+  },
+  {
+    "revision": "1099ee158fd32991f5b1",
+    "url": "/my-portfolio/static/js/runtime-main.0138d583.js"
+  }
+]);
